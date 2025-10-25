cd26ea94bfcb10f7d3171116d784621c4650ebc6
pops <nicoanton.gering@gmail.com>
2025-10-26 00:21:22 +0200

    Add instruction templates

---
+++ lib/cchip8ins.c
@@ -0,0 +1,40 @@
+#include "cchip8.h"
+#include "cchip8ins.h"
+
+// This instruction is ignored by modern interpreters
+void Chip8_0NNN(Chip8 *chip8) {}; 
+
+void Chip8_00E0(Chip8 *chip8) {}; 
+void Chip8_00EE(Chip8 *chip8) {};
+void Chip8_1NNN(Chip8 *chip8) {};
+void Chip8_2NNN(Chip8 *chip8) {};
+void Chip8_3XKK(Chip8 *chip8) {};
+void Chip8_4XKK(Chip8 *chip8) {};
+void Chip8_5XY0(Chip8 *chip8) {};
+void Chip8_6XKK(Chip8 *chip8) {};
+void Chip8_7XKK(Chip8 *chip8) {};
+void Chip8_8XY0(Chip8 *chip8) {};
+void Chip8_8XY1(Chip8 *chip8) {};
+void Chip8_8XY2(Chip8 *chip8) {};
+void Chip8_8XY3(Chip8 *chip8) {};
+void Chip8_8XY4(Chip8 *chip8) {};
+void Chip8_8XY5(Chip8 *chip8) {};
+void Chip8_8XY6(Chip8 *chip8) {};
+void Chip8_8XY7(Chip8 *chip8) {};
+void Chip8_8XYE(Chip8 *chip8) {};
+void Chip8_9XY0(Chip8 *chip8) {};
+void Chip8_ANNN(Chip8 *chip8) {};
+void Chip8_BNNN(Chip8 *chip8) {};
+void Chip8_CXKK(Chip8 *chip8) {};
+void Chip8_DXYN(Chip8 *chip8) {};
+void Chip8_EX9E(Chip8 *chip8) {};
+void Chip8_EXA1(Chip8 *chip8) {};
+void Chip8_FX07(Chip8 *chip8) {};
+void Chip8_FX0A(Chip8 *chip8) {};
+void Chip8_FX15(Chip8 *chip8) {};
+void Chip8_FX18(Chip8 *chip8) {};
+void Chip8_FX1E(Chip8 *chip8) {};
+void Chip8_FX29(Chip8 *chip8) {};
+void Chip8_FX33(Chip8 *chip8) {};
+void Chip8_FX55(Chip8 *chip8) {};
+void Chip8_FX65(Chip8 *chip8) {}; 
