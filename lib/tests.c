ab2f8bd9152e98d9232d26986530b24436461151
POP303U <nicoanton.gering@gmail.com>
2025-10-27 20:57:56 +0100

    Fix for Chip8

--- lib/tests.c
+++ lib/tests.c
@@ -1,3 +1,4 @@
+#include "cchip8.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -6,17 +7,16 @@
 #define FB_WIDTH 64
 #define FB_HEIGHT 32
 
-void TEST_framebuffer_smile(uint8_t framebuffer[]) {
-  framebuffer[64 * 16 + 28] = true;
-  framebuffer[64 * 16 + 32] = true;
-
-  framebuffer[64 * 19 + 27] = true;
-  framebuffer[64 * 20 + 28] = true;
-  framebuffer[64 * 20 + 29] = true;
-  framebuffer[64 * 20 + 30] = true;
-  framebuffer[64 * 20 + 31] = true;
-  framebuffer[64 * 20 + 32] = true;
-  framebuffer[64 * 19 + 33] = true;
+void TEST_framebuffer_smile(Chip8 *chip8) {
+  chip8->framebuffer[64 * 16 + 28] = true;
+  chip8->framebuffer[64 * 16 + 32] = true;
+  chip8->framebuffer[64 * 19 + 27] = true;
+  chip8->framebuffer[64 * 20 + 28] = true;
+  chip8->framebuffer[64 * 20 + 29] = true;
+  chip8->framebuffer[64 * 20 + 30] = true;
+  chip8->framebuffer[64 * 20 + 31] = true;
+  chip8->framebuffer[64 * 20 + 32] = true;
+  chip8->framebuffer[64 * 19 + 33] = true;
 
   // If fb looping works: Framebuffer index 2047 => x=63, y=31
   printf("Framebuffer index 2047 => x=%d, y=%d\n", 2047 % FB_WIDTH,
--- lib/tests.h
+++ lib/tests.h
@@ -1,10 +1,11 @@
+#include "cchip8.h"
 #include <stdbool.h>
 #include <stdint.h>
 
 #ifndef TESTS_H_
 #define TESTS_H_
 
-void TEST_framebuffer_smile(uint8_t framebuffer[]);
+void TEST_framebuffer_smile(Chip8 *chip8);
 // void fbSetPixel(uint8_t fb[], int x, int y, bool pixel);
 void romToBin(uint8_t *buf, int bytes_to_read);
 
